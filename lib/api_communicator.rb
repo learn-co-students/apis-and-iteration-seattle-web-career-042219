99ff54292ce505612ac5270bc021c664767cdfd4
Danny Tseng <github email address>
2019-04-24 16:06:09 -0700

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,34 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def helper_get_character_films(results, character_name)
+  character_films = []
+  results.each do |character|
+    if character["name"].downcase == character_name
+      character_films = character["films"]
+    end
+  end
+  character_films
+end
+
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  response_hash = JSON.parse(RestClient.get('http://www.swapi.co/api/people/'))
+  results = response_hash["results"]
+  character_films = helper_get_character_films(results, character_name)
+  character_films.map do |request|
+    JSON.parse(RestClient.get(request))
+  end
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |film|
+    puts "Star Wars #{film["title"]}, Episode #{film["episode_id"]}"
+    puts "Directed by #{film["director"]}, Produced by #{film["producer"]}"
+    puts "Release date: #{film["release_date"]}"
+    puts
+  end
 end
 
 def show_character_movies(character)
@@ -28,6 +38,7 @@ def show_character_movies(character)
 end
 
 ## BONUS
+
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?

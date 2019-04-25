89b2e37a8abb8fc1007cfbb1076177e1acdaa049
Flatiron School <jjkim429@gmail.com>
2019-04-24 17:09:36 -0700

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,7 +3,6 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
@@ -16,10 +15,39 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+
+  test = response_hash["results"]
+  test.each do |imdb|
+    info = imdb["name"]
+    if info == character_name
+      imdb["films"]
+    end
+  end
+  #this is an array of hashes
+  #movies = level_above_movies["films"]
+  #characters = level_above_movies["name"]
+  #this is an array
+  #character = character_name
+
+  # level_above_movies.each do |array|
+  #   array.each do |key, value|
+  #     if value == character_name
+
+
+  #     end
+  #   end
+  # end
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+    # [
+    # https://www.swapi.co/api/films/2/
+    # https://www.swapi.co/api/films/6/
+    # https://www.swapi.co/api/films/3/
+    # https://www.swapi.co/api/films/1/
+    # https://www.swapi.co/api/films/7/
+    # ]
 end
 
 def show_character_movies(character)
